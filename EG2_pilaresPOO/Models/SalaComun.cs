6941a2e55603cb5a46741b06f4a7da79aa49848d
Betza <betza.otaku.karla@gmail.com>
2025-08-06 21:56:14 -0600

    Ejercicio Implementacion sala

--- EG2_pilaresPOO/Models/SalaComun.cs
+++ EG2_pilaresPOO/Models/SalaComun.cs
@@ -1,11 +1,24 @@
 namespace EG2_pilaresPOO.Models;
 public class SalaComun : Sala
 {
+    public int Mesas { get; set; } = 10;
+    public int Sillas { get; set; } = 20;
+    public bool TieneCanon { get; set; } = true;
+    public bool TienePizarron { get; set; } = true;
+    public bool TieneAireAcondicionado { get; set; } = true;
     public SalaComun(string nombre, int capacidad, string ubicacion) : base(nombre, capacidad, ubicacion) { }
 
     public override bool Reservar(Evento evento)
     {
-         
-        return true;
+        int hora = evento.Fecha.Hour;
+
+        if (hora >= 8 && hora <= 18)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
