f0b21011d42de99b38e9062cebaba31ea6d0364a
Jennifer-dg <dgaby0214@gmail.com>
2025-08-06 22:18:35 -0600

    Cambios eventos

--- EG2_pilaresPOO/Models/Evento.cs
+++ EG2_pilaresPOO/Models/Evento.cs
@@ -2,16 +2,29 @@ namespace EG2_pilaresPOO.Models;
 public class Evento
 {
     public string Nombre { get; set; }
-    public DateTime Fecha { get; set; } //podes dejarlo como datetime o dejarlo como un int, validar que sea superior a >0
+    public DateTime Fecha { get; set; } 
     public string Tipo { get; set; }
     private string ContactoOrganizador { get; set; }
     public int Capacidad { get; internal set; }
 
     public Evento(string nombre, DateTime fecha, string tipo, string contactoOrganizador)
     {
-        Nombre = nombre; // que no sea nulo o espacios
-        Fecha = fecha; // Validar que la fecha tenga un buen formato
-        Tipo = tipo; //Validar que el evento solo sea academico o extracurricular
-        ContactoOrganizador = contactoOrganizador; // que no sea nulo o espacios
+        if (string.IsNullOrWhiteSpace(nombre))
+            throw new ArgumentException("El nombre no puede estar vacío");
+
+        if (fecha == default || fecha < DateTime.Today)
+            throw new ArgumentException("La fecha tiene que ser válida ");
+
+        string tipoLower = tipo.ToLower();
+        if (tipoLower != "academico" && tipoLower != "extracurricular")
+            throw new ArgumentException("El tipo de evento debe ser 'academico' o 'extracurricular'");
+
+        if (string.IsNullOrWhiteSpace(contactoOrganizador))
+            throw new ArgumentException("El contacto del organizador no puede estar vacío.");
+
+        Nombre = nombre; 
+        Fecha = fecha; 
+        Tipo = tipo; 
+        ContactoOrganizador = contactoOrganizador; 
     }
 }
